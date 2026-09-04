00f4829ca8a7195b35aefdba3d58f1d14e156b96
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-04 06:44:44 +0000

    leetie: Add 274. H-Index [Medium] (java)

--- solutions/Medium/0274-h-index/solution.java
+++ solutions/Medium/0274-h-index/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Sorting, Counting Sort
 // Link     : https://leetcode.com/problems/h-index/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42112000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43564000 (beats 42%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
