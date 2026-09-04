da21bf82e8f89d1ab43e08371d60b4aa9a87f3b0
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-04 06:56:41 +0000

    leetie: Add 435. Non-overlapping Intervals [Medium] (java)

--- solutions/Medium/0435-non-overlapping-intervals/solution.java
+++ solutions/Medium/0435-non-overlapping-intervals/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Greedy, Sorting
 // Link     : https://leetcode.com/problems/non-overlapping-intervals/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42736000 (beats 0%)
+// Runtime  : 50 ms (beats 22%)
+// Memory   : 115788000 (beats 57%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
