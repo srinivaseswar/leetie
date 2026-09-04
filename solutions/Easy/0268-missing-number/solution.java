5eeefa3a729210728dd9047436b4317f292683f8
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-04 06:43:51 +0000

    leetie: Add 268. Missing Number [Easy] (java)

--- solutions/Easy/0268-missing-number/solution.java
+++ solutions/Easy/0268-missing-number/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Math, Binary Search, Bit Manipulation, Sorting
 // Link     : https://leetcode.com/problems/missing-number/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42468000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 47156000 (beats 82%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
