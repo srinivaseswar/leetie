220760fa6582472157942600194078b163d94c74
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-05 16:09:38 +0000

    leetie: Add 209. Minimum Size Subarray Sum [Medium] (java)

--- solutions/Medium/0209-minimum-size-subarray-sum/solution.java
+++ solutions/Medium/0209-minimum-size-subarray-sum/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search, Sliding Window, Prefix Sum
 // Link     : https://leetcode.com/problems/minimum-size-subarray-sum/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42744000 (beats 0%)
+// Runtime  : 1 ms (beats 100%)
+// Memory   : 69124000 (beats 67%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
