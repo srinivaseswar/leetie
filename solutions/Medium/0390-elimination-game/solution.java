3f753132cfe840ee71e993ac58b76811ace787fe
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-06 17:21:36 +0000

    leetie: Add 390. Elimination Game [Medium] (java)

--- solutions/Medium/0390-elimination-game/solution.java
+++ solutions/Medium/0390-elimination-game/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Recursion
 // Link     : https://leetcode.com/problems/elimination-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42344000 (beats 0%)
+// Runtime  : 2 ms (beats 93%)
+// Memory   : 43440000 (beats 45%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
