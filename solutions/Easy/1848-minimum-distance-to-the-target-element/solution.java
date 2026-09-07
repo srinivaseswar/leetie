f9144e5ea783caa3fe7642a14ec39fafde134ca4
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-07 03:18:42 +0000

    leetie: Add 1848. Minimum Distance to the Target Element [Easy] (java)

--- solutions/Easy/1848-minimum-distance-to-the-target-element/solution.java
+++ solutions/Easy/1848-minimum-distance-to-the-target-element/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/minimum-distance-to-the-target-element/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42492000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 44960000 (beats 73%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
