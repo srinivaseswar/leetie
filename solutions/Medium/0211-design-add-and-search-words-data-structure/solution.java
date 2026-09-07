d438a2bebd4735a3b30c96d2d2717c6b1e9463d8
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-07 03:39:31 +0000

    leetie: Add 211. Design Add and Search Words Data Structure [Medium] (java)

--- solutions/Medium/0211-design-add-and-search-words-data-structure/solution.java
+++ solutions/Medium/0211-design-add-and-search-words-data-structure/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : String, Depth-First Search, Design, Trie
 // Link     : https://leetcode.com/problems/design-add-and-search-words-data-structure/
-// Runtime  : 3 ms (beats 0%)
-// Memory   : 42680000 (beats 0%)
+// Runtime  : 231 ms (beats 31%)
+// Memory   : 274156000 (beats 66%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
