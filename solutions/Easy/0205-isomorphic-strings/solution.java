43c6b83137abb2611c2d286a87d1f8b7eebf070a
srinivaseswar <srinivaseswar@users.noreply.github.com>
2026-09-07 03:35:38 +0000

    leetie: Add 205. Isomorphic Strings [Easy] (java)

--- solutions/Easy/0205-isomorphic-strings/solution.java
+++ solutions/Easy/0205-isomorphic-strings/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/isomorphic-strings/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42628000 (beats 0%)
+// Runtime  : 6 ms (beats 89%)
+// Memory   : 43812000 (beats 58%)
 // Language : java
 // Copyright: (c) 2026 srinivaseswar. All rights reserved.
 // Synced by: leetie
